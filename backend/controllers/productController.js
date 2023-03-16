9fb12ecc3c984167e5b7a5e29acfe64df2fa429b
Nayan Prasad P K <nayanprasad096@gmail.com>
2023-03-16 23:30:14 +0530

    added product api (CRUD)

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,91 @@
+const Product = require("../models/productModel");
 
-exports.getAllProducts = (req, res) => {
 
-  res.status(200).json({message: "route is working"});
+// Create new product   --Admin
+exports.createProduct = async (req, res, next) => {
 
+  const product = await Product.create(req.body);
+
+  res.status(200).json({
+    success: true,
+    product
+  })
+
+};
+
+// Get all products
+exports.getAllProducts = async (req, res, next) => {
+
+  const products = await Product.find()
+
+  res.status(200).json({
+    success: true,
+    products
+  });
+}
+
+// Get single product details
+
+exports.getSingleProduct = async (req, res, next) => {
+
+  const product = await Product.findById(req.params.id);
+
+  if(!product) {
+    return res.status(404).json({
+      success: false,
+      message: "Product not found"
+    })
+  }
+
+  res.status(200).json({
+    success: true,
+    product
+  })
+}
+
+
+exports.updateProduct = async (req, res, next) => {
+  
+  const product = Product.findById(req.params.id);
+
+  if(!product) {
+    return res.status(404).json({
+      success: false,
+      message: "Product not found"
+    })
+  }
+
+  const updatedProduct = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true, // This will return the updated product
+    runValidators: true, // This will validate the data
+    useFindAndModify: false // This will use the new method of updating
+  })
+
+  res.status(200).json({
+    success: true,
+    updatedProduct
+
+  })
+};
+
+
+
+// Delete product  --Admin
+exports.deleteProduct = async (req, res, next) => {
+
+  const product = await Product.findById(req.params.id);
+
+  if(!product) {
+    return res.status(404).json({
+      success: false,
+      message: "Product not found"
+    })
+  }
+
+  await Product.findByIdAndDelete(req.params.id);  // since product.remove() is deprecated
+
+  res.status(200).json({
+    success: true,
+    message: "Product is deleted",
+  })
 }
--- backend/routes/productRouter.js
+++ backend/routes/productRouter.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { getAllProducts } = require("../controllers/productController");
+const { getAllProducts, createProduct, updateProduct, deleteProduct, getSingleProduct } = require("../controllers/productController");
 
 
 router.route("/products").get(getAllProducts);
+router.route("/product/new").post(createProduct);
+router.route("/product/:id").get(getSingleProduct).put(updateProduct).delete(deleteProduct);
 
 
 module.exports = router;
+
