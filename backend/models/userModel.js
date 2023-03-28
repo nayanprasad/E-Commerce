931f21f7bde0b94461ff24d039dd3117f431083a
Nayan Prasad P K <nayanprasad096@gmail.com>
2023-03-29 02:30:51 +0530

    added password bcrypt

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
+
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -43,6 +45,13 @@ const userSchema = new mongoose.Schema({
   resetPasswordExpire: Date,
 
 });
+
+
+userSchema.pre("save", async function(next) {
+  if(!this.isModified("password"))
+    next();
+  this.password = await bcrypt.hash(this.password, 10);
+})
 
 
 module.exports = mongoose.model('User', userSchema);
