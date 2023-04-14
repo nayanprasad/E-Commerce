58179ac3c51a14bd3a9c705e36bb3936194c4509
Nayan Prasad P k <nayanprasad096@gmail.com>
2023-04-15 03:29:58 +0530

    updated some error showing

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -10,6 +10,24 @@ module.exports = (err, req, res, next) => {
     err = new ErrorHandler(message, 400);
   }
 
+  //mongoose duplicate key error
+  if(err.code === 11000) {
+    const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  // wrong jwt error
+  if(err.message === "jsonWebTokenError") {
+    const message = `json web token is invalid, try again`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  // jwt expire error
+  if(err.message === "TokenExpiredError") {
+    const message = `json web token is expired, try again`;
+    err = new ErrorHandler(message, 400);
+  }
+
   res.status(err.statuscode).json({
     success: false,
     // error: err.stack  //this will tell the exact position where the error occure
--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -6,7 +6,7 @@ const {registerUser, loginUser, logout, forgotPassword, resetPassword} = require
 Router.route("/register").post(registerUser);
 Router.route("/login").post(loginUser);
 Router.route("/logout").get(logout);
-Router.route("/password/forgot").post(forgotPasswordassword);
+Router.route("/password/forgot").post(forgotPassword);
 Router.route("/password/reset/:token").put(resetPassword);
 
 module.exports = Router;
