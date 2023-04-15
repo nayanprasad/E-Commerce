71e4fedca810c15ce9402abc728dbb3fd36ea0f8
Nayan Prasad P k <nayanprasad096@gmail.com>
2023-04-15 17:47:24 +0530

    added get all users for admin

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const Router = express.Router();
-const {registerUser, loginUser, logout, forgotPassword, resetPassword, getUser, updatePassword, updateProfile} = require("../controllers/userController");
-const {isAuthenticatedUser} = require("../middleware/auth");
+const {registerUser, loginUser, logout, forgotPassword, resetPassword, getUser, updatePassword, updateProfile,
+    getAllUsers
+} = require("../controllers/userController");
+const {isAuthenticatedUser, isAuthorizedRoles} = require("../middleware/auth");
 
 
 Router.route("/register").post(registerUser);
@@ -12,6 +14,7 @@ Router.route("/password/reset/:token").put(resetPassword);
 Router.route("/me").get(isAuthenticatedUser, getUser);
 Router.route("/password/update").put(isAuthenticatedUser, updatePassword);
 Router.route("/me/update").put(isAuthenticatedUser, updateProfile)
+Router.route("/admin/users").get(isAuthenticatedUser, isAuthorizedRoles("admin"), getAllUsers);
 
 module.exports = Router;
 
