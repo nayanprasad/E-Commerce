bd322331c216803339aa6e6538bdcd821a77c641
Nayan Prasad P k <nayanprasad096@gmail.com>
2023-05-12 16:18:54 +0530

    added Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,40 @@
-import './App.css';
+import React from "react";
+import {
+    BrowserRouter as Router,
+    // Switch,
+    // Route,
+    // Link,
+    // Routes
+} from "react-router-dom";
+import Header from "./components/layouts/Header";
 
-function App() {
-  return (
-    <div>
 
-    </div>
-  );
+export default function App() {
+    return (
+        <Router>
+            <div>
+                <Header/>
+                {/*<Routes>*/}
+                {/*    <Route path="/" element={<Join getName={getName} showAlert={showAlert}/>}/>*/}
+                {/*    <Route path="/chat" element={<Chat name={name}/>}/>*/}
+                {/*</Routes>*/}
+            </div>
+        </Router>
+    );
 }
 
-export default App;
+function Home() {
+    return <h2>Home</h2>;
+}
+
+function About() {
+    return <h2>About</h2>;
+}
+
+function Blogs() {
+    return <h2>Blogs</h2>;
+}
+
+function NoPage() {
+    return <h2>404</h2>;
+}
