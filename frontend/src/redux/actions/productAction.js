cfd4c27277d7d3f2b93926c5c203417b541fb095
nayanprasad <nayanprasad096@gmail.com>
2023-05-20 17:22:32 +0530

    create product action

---
+++ frontend/src/redux/actions/productAction.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+import {
+    PRODUCT_LIST_REQUEST,
+    PRODUCT_LIST_SUCCESS,
+    PRODUCT_LIST_FAILS } from "../../constants/productConstant";
+
+export const listProduct = async (dispatch) => {
+    try{
+        dispatch({type: PRODUCT_LIST_REQUEST});
+
+        const response = await axios({
+            method: "GET",
+            url: "http://localhost:3000/api/v1/products"
+        });
+
+        dispatch({
+            type: PRODUCT_LIST_SUCCESS,
+            payload: response.data
+        })
+
+    }catch (err) {
+        dispatch({
+            type: PRODUCT_LIST_FAILS ,
+            payload: err
+        })
+    }
+}
