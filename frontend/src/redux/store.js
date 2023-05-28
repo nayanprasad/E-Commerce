f5bc048605251f4e0af20e2b964fb46b51be2f6e
Nayan Prasad P K <93549213+nayanprasad@users.noreply.github.com>
2023-05-28 23:29:14 +0530

    added product detail reducer in store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,11 +1,12 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import {productListReducer} from "./reducers/productReducer";
+import {productDetailsReducer, productListReducer} from "./reducers/productReducer";
 
 
 const reducer = combineReducers({
-    productList: productListReducer
+    productList: productListReducer,
+    productDetails: productDetailsReducer
 
 });
 
