6a6ed010951d48cc0a22d98e740b3220da29009f
Nayan Prasad P K <93549213+nayanprasad@users.noreply.github.com>
2023-06-03 03:41:53 +0530

    fix loading for long time for home page

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -30,7 +30,7 @@ const Home = () => {
     const {loading, error, products} = productList;
 
     useEffect(() => {
-        dispatch(listProducts);
+        dispatch(listProducts());
 
     }, [dispatch]);
 
