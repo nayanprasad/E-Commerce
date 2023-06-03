b87aaf2c2f12f92fce0f259c6ac8459f1c1cd05a
Nayan Prasad P K <93549213+nayanprasad@users.noreply.github.com>
2023-06-03 16:16:31 +0530

    created signup action

--- frontend/src/redux/actions/userAction.js
+++ frontend/src/redux/actions/userAction.js
@@ -3,6 +3,9 @@ import {
     LOGIN_SUCCESS,
     LOGIN_REQUEST,
     LOGIN_FAILS,
+    SIGNUP_SUCCESS,
+    SIGNUP_REQUEST,
+    SIGNUP_FAILS,
 } from "../constants/userConstant";
 import {CLEAR_ERRORS} from "../constants/productConstant";
 
@@ -38,6 +41,33 @@ export const login = (email, password) => async (dispatch) => {
         })
     }
 }
+
+export const signup = (userData) => async (dispatch) => {
+    try {
+
+        dispatch({type: SIGNUP_REQUEST})
+
+        const {data} = await axios({
+            method: "POST",
+            data : userData,
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+
+        dispatch({
+            type: SIGNUP_SUCCESS,
+            payload: data.user
+        })
+
+    }catch (e) {
+        dispatch({
+            type: SIGNUP_FAILS,
+            payload: e.response.data.message
+        })
+    }
+}
+
 
 
 export const clearErrors = () => async (dispatch) => {
