64877c711b27b99925ac7b75645b04dc18d6f54d
Nayan Prasad P K <93549213+nayanprasad@users.noreply.github.com>
2023-06-10 07:33:43 +0530

    added a dimmed overlay on opening the usericon speed dial

--- frontend/src/components/layouts/Header/UserIcon.jsx
+++ frontend/src/components/layouts/Header/UserIcon.jsx
@@ -1,5 +1,5 @@
-import React, {Fragment, useEffect} from 'react';
-import {styled} from '@mui/material/styles';
+import React, { Fragment, useState } from 'react';
+import { styled } from '@mui/material/styles';
 import SpeedDial from '@mui/material/SpeedDial';
 import SpeedDialIcon from '@mui/material/SpeedDialIcon';
 import SpeedDialAction from '@mui/material/SpeedDialAction';
@@ -8,39 +8,51 @@ import LogoutIcon from '@mui/icons-material/Logout';
 import ListAltIcon from '@mui/icons-material/ListAlt';
 import PersonIcon from '@mui/icons-material/Person';
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
-import {useNavigate} from "react-router-dom";
-import "./UserIcon.css"
-import {useSelector, useDispatch} from "react-redux";
-import {logout} from "../../../redux/actions/userAction";
-import {toast} from "react-toastify"
+import { useNavigate } from 'react-router-dom';
+import './UserIcon.css';
+import { useDispatch } from 'react-redux';
+import { logout } from '../../../redux/actions/userAction';
+import { toast } from 'react-toastify';
 
-const StyledSpeedDial = styled(SpeedDial)(({theme}) => ({
+const StyledSpeedDial = styled(SpeedDial)(({ theme }) => ({
     position: 'fixed',
     top: '3%',
     right: theme.spacing(2),
     zIndex: 1,
 }));
 
+const DimmedOverlay = styled('div')({
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Adjust the opacity and color as needed
+    zIndex: 1,
+});
 
-export default function PlaygroundSpeedDial({user}) {
+export default function PlaygroundSpeedDial({ user }) {
 
     const navigate = useNavigate();
     const dispatch = useDispatch();
 
+    const [open, setOpen] = useState(false);
 
     const actions = [
-        {icon: <PersonIcon/>, name: 'Profile'},
-        {icon: <ListAltIcon/>, name: 'Orders',},
-        {icon: <LogoutIcon/>, name: 'Logout',},
+        { icon: <PersonIcon />, name: 'Profile' },
+        { icon: <ListAltIcon />, name: 'Orders' },
+        { icon: <LogoutIcon />, name: 'Logout' },
     ];
 
     if (user?.role === 'admin') {
-        actions.unshift({icon: <DashboardIcon/>, name: 'Dashboard'});
+        actions.unshift({ icon: <DashboardIcon />, name: 'Dashboard' });
     }
 
-    //handler function
+
     function handleClick(e, name) {
         e.preventDefault();
+        setOpen(false);
+
         switch (name) {
             case 'Profile':
                 navigate('/profile');
@@ -49,25 +61,31 @@ export default function PlaygroundSpeedDial({user}) {
                 navigate('/orders');
                 break;
             case 'Logout':
-                dispatch(logout())
-                toast.success("logout successfully")
+                dispatch(logout());
+                toast.success('Logout successfully');
                 break;
             case 'Dashboard':
                 navigate('/admin/dashboard');
-
                 break;
         }
     }
 
     return (
         <Fragment>
+            {open && <DimmedOverlay onClick={() => setOpen(false)} />}
             <StyledSpeedDial
                 ariaLabel="SpeedDial playground example"
-                hidden={false}
-                icon={user ?
-                    // <img src={user.avatar.url} alt={user.name} className="speedDialIcon" />
-                    <AccountBoxIcon/>
-                    : <AccountBoxIcon/>}
+                open={open}
+                onOpen={() => setOpen(true)}
+                onClose={() => setOpen(false)}
+                icon={
+                    user ? (
+                        // <img src={user.avatar.url} alt={user.name} className="speedDialIcon" />
+                        <AccountBoxIcon />
+                    ) : (
+                        <AccountBoxIcon />
+                    )
+                }
                 direction="down"
             >
                 {actions.map((action) => (
@@ -76,7 +94,7 @@ export default function PlaygroundSpeedDial({user}) {
                         icon={action.icon}
                         tooltipTitle={action.name}
                         onClick={(e) => {
-                            handleClick(e, action.name)
+                            handleClick(e, action.name);
                         }}
                     />
                 ))}
