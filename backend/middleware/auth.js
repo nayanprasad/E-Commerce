5ccd54c757850debf80c24a6a37f78df6a02b279
Nayan Prasad P K <93549213+nayanprasad@users.noreply.github.com>
2023-07-04 17:00:46 +0530

    updated auth middle ware

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -6,21 +6,28 @@ const User = require("../models/userModel");
 
 exports.isAuthenticatedUser = CatchAsyncErrors(async (req, res, next) => {
 
-    // let {token} = req.cookies;
 
-    if(!req.headers.authorization)
-        return next(new ErrorHandler("token not found", 401));
+    if(req.cookies.token) {
+        if(!req.headers.authorization)
+            return next(new ErrorHandler("no token found", 401));
 
-    const token = req.headers.authorization.split(" ")[1];
+        const token = req.cookies.token
 
-    if(!token || token === "j:null")
+        if(token !== req.headers.authorization.split(" ")[1])
+            return next(new ErrorHandler("Invalid token", 401));
+
+        if(!token || token === "j:null")
+            return next(new ErrorHandler("please login to access this resource", 401));
+
+        const decodedData = jwt.verify(token, process.env.JWT_SECRETE);
+
+        const user = await User.findById(decodedData.id);
+        req.user = user
+        next();
+    }
+    else {
         return next(new ErrorHandler("please login to access this resource", 401));
-
-    const decodedData = await jwt.verify(token, process.env.JWT_SECRETE);
-
-    req.user = await User.findById(decodedData.id);
-
-    next();
+    }
 });
 
 
