2c760946132741c2871b0690fb8f683c9cef4a3c
Nayan Prasad P K <93549213+nayanprasad@users.noreply.github.com>
2023-07-08 05:04:56 +0530

    data setup for shipping details form page

--- frontend/src/components/Cart/Shipping.jsx
+++ frontend/src/components/Cart/Shipping.jsx
@@ -7,71 +7,111 @@ import PhoneIcon from '@mui/icons-material/Phone';
 import PublicIcon from '@mui/icons-material/Public';
 import LanguageIcon from '@mui/icons-material/Language';
 import Stepper from "./Stepper";
-import StepperComponent from "./Stepper";
+import {useDispatch, useSelector} from "react-redux";
+import {saveShippingDetails} from "../../redux/actions/cartAction";
+import { Country, State, City }  from 'country-state-city';
+
 
 const Shipping = () => {
 
-    const [userData, setUserData] = useState({
-        name: "",
-        email: "",
-        avatar: ""
-    });
+    const dispatch = useDispatch()
+    const {shippingDetails} = useSelector(state => state.cart)
+
+    const [data, setData] = useState({
+        address: shippingDetails.address,
+        city: shippingDetails.city,
+        pinCode: shippingDetails.pinCode,
+        phone: shippingDetails.phone,
+        country: shippingDetails.country,
+        state: shippingDetails.state
+    })
+
+    const [country, setCountry] = useState(Country.getAllCountries())
+    const [state, setState] = useState(State.getAllStates())
+
+    const handleChange = (e) => {
+        setData({
+            ...data,
+            [e.target.name]: e.target.value
+        })
+
+        if(e.target.name === "country"){
+            const country = Country.getCountryByCode(e.target.value)
+            setData({
+                ...data,
+                country: country.name,
+            })
+            setState(State.getStatesOfCountry(e.target.value))
+        }
+
+        if(e.target.name === "state"){
+            setData({
+                ...data,
+                state: e.target.value,
+            })
+        }
+
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        dispatch(saveShippingDetails(data))
+    }
 
 
     return (
         <Fragment>
 
             <div className="stepperContainer">
-                <StepperComponent activeStep={0}/>
+                <Stepper activeStep={0}/>
             </div>
 
-            <div className="wrapperContainer" >
+            <div className="wrapperContainer">
                 <div className="wrapper">
                     <div className="title-text">
                         <div className="title login">Shipping details</div>
                     </div>
                     <div className="form-container">
                         <div className="form-inner">
-                            <form className="signup" >
+                            <form className="signup" onSubmit={handleSubmit}>
                                 <div className="field flex">
                                     <HomeIcon/>
-                                    <input type="text" placeholder="Address" name="name" value={userData.name}
-                                           onChange={(e) => setUserData({
-                                               ...userData,
-                                               [e.target.name]: e.target.value
-                                           })}/>
+                                    <input type="text" placeholder="Address" name="address" value={data.address}
+                                           onChange={handleChange}/>
                                 </div>
                                 <div className="field flex">
                                     <LocationCityIcon/>
-                                    <input type="text" placeholder="City" name="email"
-                                           value={userData.email} />
+                                    <input type="text" placeholder="City" name="city"
+                                           value={data.city} onChange={handleChange}/>
                                 </div>
                                 <div className="field flex">
                                     <PlaceIcon/>
-                                    <input type="text" placeholder="Pin code" name="email"
-                                           value={userData.email} />
+                                    <input type="text" placeholder="Pin code" name="pinCode"
+                                           value={data.pinCode} onChange={handleChange}/>
                                 </div>
                                 <div className="field flex">
                                     <PhoneIcon/>
-                                    <input type="text" placeholder="Phone" name="email"
-                                           value={userData.email} />
+                                    <input type="text" placeholder="Phone" name="phone"
+                                           value={data.phone} onChange={handleChange}/>
                                 </div>
                                 <div className="field flex">
                                     <PublicIcon/>
-                                    <select name="country" id="country" >
-                                        <option disabled selected value="" >Country</option>
-                                        <option value="India">India</option>
-                                        <option value="USA">USA</option>
-                                        <option value="UK">UK</option>
+                                    <select name="country" id="country" onChange={handleChange}>
+                                        <option  selected value="">Country</option>
+                                        {data.country && <option selected value={data.country}>{data.country}</option>}
+                                        {country.map((country, i) => (
+                                            <option key={i} value={country.isoCode}>{country.name}</option>
+                                        ))}
                                     </select>
                                 </div>
                                 <div className="field flex">
                                     <LanguageIcon/>
-                                    <select name="state" id="state">
-                                        <option  selected disabled value="">State</option>
-                                        <option value="India">India</option>
-                                        <option value="USA">USA</option>
-                                        <option value="UK">UK</option>
+                                    <select name="state" id="state" onChange={handleChange}>
+                                        <option selected value="">State</option>
+                                        {data.state && <option selected value={data.state}>{data.state}</option>}
+                                        {state.map((state, i) => (
+                                            <option key={i} value={state.name}>{state.name}</option>
+                                        ))}
                                     </select>
                                 </div>
 
