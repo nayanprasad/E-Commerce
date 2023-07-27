22a4c5ed54cfec70f1c7f96f403d19dbaffb7754
Nayan Prasad P K <93549213+nayanprasad@users.noreply.github.com>
2023-07-28 00:15:27 +0530

    feat: moved to node-requset-logger desc: package made by me

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const core = require("cors")
 const fileUpload = require("express-fileupload")
 const bodyParser = require("body-parser")
 const dotenv = require("dotenv");
+const requestLogger = require("node-requset-logger");
 
 dotenv.config({ path: 'backend/config/config.env' })
 
@@ -15,71 +16,7 @@ app.use(express.json()); // This is a middleware that allows us to accept json d
 app.use(cookieParse());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(fileUpload());
-// app.use(morgan('combined'));
-
-
-// ANSI escape sequences for colors and styles
-const RESET = '\x1b[0m';
-const LIGHT_GREEN = '\x1b[92m';
-const YELLOW = '\x1b[33m';
-const LIGHT_BLUE = '\x1b[94m';
-const RED = '\x1b[31m';
-const GRAYISH = '\x1b[37m';
-
-// Middleware to log incoming requests with colored method and URL
-app.use((req, res, next) => {
-    const arrow = `${GRAYISH}==> `;
-    const timestamp = new Date().toISOString();
-    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    const method = req.method;
-    const url = req.url;
-
-    let methodColor;
-    switch (method) {
-        case 'GET':
-            methodColor = LIGHT_GREEN;
-            break;
-        case 'POST':
-            methodColor = YELLOW;
-            break;
-        case 'DELETE':
-            methodColor = RED;
-            break;
-        case 'PUT':
-            methodColor = LIGHT_BLUE;
-            break;
-        default:
-            methodColor = RESET;
-    }
-
-    const formattedTimestamp = new Date(timestamp).toLocaleString('en-US', {
-        year: 'numeric',
-        month: 'short',
-        day: 'numeric',
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit',
-        hour12: false,
-    });
-
-    console.log(`\n${arrow}[${formattedTimestamp}] Incoming request: ${methodColor}${method}${RESET} ${GRAYISH}${url}${RESET} from ${ip}`);
-    // console.log(`${GRAYISH}Headers:${RESET}`, req.headers);
-    // console.log(`${GRAYISH}Body:${RESET}`, req.body);
-
-    res.on('finish', () => {
-        const statusCode = res.statusCode;
-        let statusColor;
-        if (statusCode >= 300) {
-            statusColor = RED;
-        } else {
-            statusColor = LIGHT_GREEN;
-        }
-        console.log(`${GRAYISH}[${formattedTimestamp}] Response status to ${methodColor}${method}${RESET} ${GRAYISH}${url}${RESET}: ${statusColor}${statusCode}${RESET}`);
-    });
-
-    next();
-});
-
+app.use(requestLogger());
 
 
 const productRouter = require("./routes/productRouter");
@@ -92,7 +29,7 @@ app.use("/api/v1", userRouter);
 app.use("/api/v1", orderRouter);
 app.use("/api/v1", paymentRouter);
 
-// middleware for errors
+
 app.use(errorMiddleware);
 
 
